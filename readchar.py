b9d9478683aa5342c43c938a26adb998650aecd2
Andreas Botero <andreas.botero@gmail.com>
2015-09-08 18:56:02 -0500

    coded some more on real machine method

--- readchar.py
+++ readchar.py
@@ -47,11 +47,41 @@ def real_machine(line, forward_p):
     error = []  # on here we are going to store every error we get
     xx_counter = 0  # counter for xx
     yy_counter = 0  # counter for yy
-
+    my_string = ""
     current_char = line[forward_p] # current didigit
+    # check for leading zero
+    if current_char == '0' and line[forward_p+1].isdigit():
+        error.append(error_list.get(7))
 
     # check if first character is a digit
-    
+    if current_char.isdigit():
+        while current_char.isdigit():
+            # add char to string
+            my_string += current_char
+            # add one to xx counter
+            xx_counter += 1
+            if xx_counter > 5:  # here we are checking if xx counter goes over limit
+                error.append(error_list.get(5))  # if it does then we add error to the error array
+            if current_char is '.':
+                forward_p += 1  # increase pointer
+                my_string += current_char # add current character to the string
+                # get next character
+                # get the next character in the line
+                current_char = line[forward_p]
+                while current_char.isdigit():
+                    # add 1 to the yy counter
+                    yy_counter += 1
+                    # add current char to the string
+                    my_string += current_char
+                    # increment pointer
+                    forward_p += 1
+                    current_char = line[forward_p]
+                    if yy_counter > 5:
+                        error.append(error_list.get(8))  # if true then add error to array
+
+    else:
+        return False
+
 
 def id_machine(string_line, forward_p):
     word = ""
