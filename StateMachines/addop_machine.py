d9def2d8c1cb6ef15f2e36c230b9fa95391528ed
Andreas Botero <andreas.botero@gmail.com>
2015-10-04 17:39:02 -0500

    finished molup and addop

--- StateMachines/addop_machine.py
+++ StateMachines/addop_machine.py
@@ -2,6 +2,9 @@ __author__ = 'andreasbotero'
 
 token = {
     1: ('ADDOP', 'Add'),
+    2: ('ADDOP', 'sub'),
+    3: ('ADDOP', 'and'),
+    4: ('ADDOP', 'or')
 
 }
 
@@ -9,13 +12,18 @@ token = {
 def addop(line, forward_point):
     current_char = line[forward_point]
     word = ""
-    if current_char is '+' or current_char is '-':
+    if current_char is '+':
         forward_point += 1
         return True, forward_point, token[1]
+    if current_char is '-':
+        forward_point += 1
+        return True, forward_point, token[2]
     elif current_char.isalpha():
         while current_char.isalpha():
             forward_point += 1
             word += current_char
-        if word is 'or' or word is 'and':
-            return True, forward_point, token[1]
+        if word is 'or':
+            return True, forward_point, token[4]
+        if word is 'and':
+            return True, forward_point, token[3]
         return False, forward_point, None
--- StateMachines/mulop_machine.py
+++ StateMachines/mulop_machine.py
@@ -1,6 +1,34 @@
 __author__ = 'andreasbotero'
 
 
+token = {
+    1: ('MULOP', 'Multiplication'),
+    2: ('MULOP', 'Division'),
+    3: ('MULOP', 'div'),
+    4: ('MULOP', 'or'),
+    5: ('MULOP', 'mod'),
+    6: ('MULOP', 'and')
+
+}
+
+
 def mulup(line, forward_p):
 
-    return 0
+    current_char = line[forward_p]
+    word = ""
+    if current_char is '*':
+        forward_p += 1
+        return True, forward_p, token[1]
+    if current_char is '/':
+        forward_p += 1
+        return True, forward_p, token[2]
+    if current_char.isalpha():
+        while current_char.isalpha():
+            forward_p += 1
+            word += current_char
+        if word is 'div':
+            return True, forward_p, token[2]
+        if word is 'mod':
+            return True, forward_p, token[5]
+        if word is 'and':
+            return True, forward_p, token[6]
